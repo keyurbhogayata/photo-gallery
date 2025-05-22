dc6f396c25fd577fb7cc0757ce6a7f14fa478d24
Keyur Bhogayata <keyur.bhogayata@veloxcore.com>
2025-05-22 12:12:45 +0530

    Uprgaded the angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,37 +11,29 @@ import { FooterComponent } from './components/footer/footer.component';
 import { ImageViewComponent } from './components/image-view/image-view.component';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 import { InMemoryDataService } from './services/in-memory-data.service'; 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { FormsModule } from '@angular/forms';
 import { CategoriesComponent } from './components/categories/categories.component';
 import { AddImageComponent } from './components/add-image/add-image.component';
-@NgModule({
-  declarations: [
-    AppComponent,
-    GalleryDashboardComponent,
-    NavbarComponent,
-    ImageGridComponent,
-    FooterComponent,
-    ImageViewComponent,
-    AboutComponent,
-    NotfoundComponent,
-    CategoriesComponent,
-    AddImageComponent,
-  ],
-  imports: [
-    FormsModule,
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false })
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
-  exports: [RouterModule]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        GalleryDashboardComponent,
+        NavbarComponent,
+        ImageGridComponent,
+        FooterComponent,
+        ImageViewComponent,
+        AboutComponent,
+        NotfoundComponent,
+        CategoriesComponent,
+        AddImageComponent,
+    ],
+    bootstrap: [AppComponent],
+    exports: [RouterModule], imports: [FormsModule,
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false })], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
